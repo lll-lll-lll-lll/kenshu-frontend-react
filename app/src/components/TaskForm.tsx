a9510fe9b1b629f6faa2e6a142b760ab5d8439a8
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-16 17:35:18 +0900

    prfix formで実装

--- app/src/components/TaskForm.tsx
+++ app/src/components/TaskForm.tsx
@@ -1,17 +1,14 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { updateTask } from "../api/task";
-import { useForm } from "@tanstack/react-form";
 import TaskButton from "./TaskButton";
 
 export const TaskForm: React.FC<{ task: TaskData }> = ({ task }) => {
   const queryClient = useQueryClient();
-  const form = useForm({
-    onSubmit: async ({ value }: { value: { title: string } }) => {
-      setTitle(value.title);
-      updateTaskMutation.mutate({ id: task.id, title });
-    },
-  });
+  const onClickSubmit = (e) => {
+    e.preventDefault();
+    updateTaskMutation.mutate({ id: task.id, title: task.title });
+  };
   const [isEditing, setIsEditing] = useState(false);
   const [title, setTitle] = useState(task.title);
   const updateTaskMutation = useMutation({
@@ -25,53 +22,40 @@ export const TaskForm: React.FC<{ task: TaskData }> = ({ task }) => {
   return (
     <form
       onSubmit={(e) => {
-        // 保存ボタン実行時にフォームのsubmitを実行する
-        if (!isEditing) {
-          e.preventDefault();
-          form.handleSubmit();
-        }
-        e.preventDefault();
-        e.stopPropagation();
+        onClickSubmit(e);
       }}
     >
-      <div>
-        <form.Field
-          name="title"
-          validators={{
-            onChange: ({ value }) => (!value ? title : ""),
-          }}
-          children={(field) => {
-            return (
-              <>
-                {isEditing ? (
-                  <input
-                    id={task.id}
-                    name={field.name}
-                    value={field.state.value || title}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                ) : (
-                  title
-                )}
-              </>
-            );
+      {isEditing ? (
+        <input
+          id={task.id}
+          name={"task"}
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
           }}
         />
-      </div>
-      <form.Subscribe
-        selector={(state) => [state.canSubmit]}
-        children={() => (
-          <TaskButton
-            type="submit"
-            onClick={() => {
-              setIsEditing(!isEditing);
-            }}
-          >
-            {isEditing ? "保存" : "編集"}
-          </TaskButton>
-        )}
-      />
+      ) : (
+        title
+      )}
+      {isEditing ? (
+        <TaskButton
+          type="submit"
+          onClick={() => {
+            setIsEditing(!isEditing);
+          }}
+        >
+          保存
+        </TaskButton>
+      ) : (
+        <TaskButton
+          type="button"
+          onClick={() => {
+            setIsEditing(!isEditing);
+          }}
+        >
+          編集
+        </TaskButton>
+      )}
     </form>
   );
 };
