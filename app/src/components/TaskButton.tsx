e53cd3dc4807455eb8d9bbda0721c09b0b8af19b
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-16 16:40:44 +0900

    fix add color to TaskButton Com

--- app/src/components/TaskButton.tsx
+++ app/src/components/TaskButton.tsx
@@ -1,12 +1,14 @@
 type TaskManagementButtonProps = {
   type: "button" | "submit" | "reset";
-  onClick: () => void; // 作成、更新、削除の処理を行う関数
+  disabled?: boolean;
+  onClick?: () => void; // 作成、更新、削除の処理を行う関数
   children: React.ReactNode;
 };
 
 const TaskButton: React.FC<TaskManagementButtonProps> = (props) => {
   return (
     <button
+      className="bg-blue-600 font-medium p-3 rounded-sm text-white"
       type={props.type}
       onClick={() => {
         props.onClick();
