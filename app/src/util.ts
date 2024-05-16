6c8819d2cee9857512a4c804775a896834560af4
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-16 12:52:29 +0900

    fix header

--- app/src/util.ts
+++ app/src/util.ts
@@ -1,7 +1,7 @@
 export const fetchTasks = async () => {
   const res: { tasks: TaskData[] } = await fetch(
     "http://localhost:8000/api/tasks",
-    { method: "GET" }
+    { method: "GET", headers: { "Content-Type": " application/json" } }
   ).then((r) => r.json());
   return res;
 };
