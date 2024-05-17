1e5441d26f28ada7f90e6190aed0db8faa67f864
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-17 12:47:28 +0900

    feat タスクFormに移動

--- app/src/components/TaskForm.tsx
+++ app/src/components/TaskForm.tsx
@@ -3,9 +3,10 @@ import TaskButton from "./TaskButton";
 import { useTaskMutation } from "../hook/task";
 
 export const TaskForm: React.FC<{ task: TaskData }> = ({ task }) => {
+  const [isDone, setIsDone] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [title, setTitle] = useState(task.title);
-  const { updateTaskMutation } = useTaskMutation();
+  const { updateTaskMutation, updateTaskStatusMutation } = useTaskMutation();
   function onClickSubmit(e) {
     e.preventDefault();
     updateTaskMutation
@@ -18,37 +19,70 @@ export const TaskForm: React.FC<{ task: TaskData }> = ({ task }) => {
       });
   }
 
+  function onClickDoneTask() {
+    updateTaskStatusMutation
+      .mutateAsync({
+        id: task.id,
+        finishedAt: new Date().toISOString(),
+      })
+      .then(() => {
+        setIsDone(!isDone);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   return (
-    <form
-      onSubmit={(e) => {
-        onClickSubmit(e);
-      }}
-    >
-      {isEditing ? (
-        <input
-          id={task.id}
-          name={"task"}
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-      ) : (
-        title
-      )}
-      {isEditing ? <TaskButton type="submit">保存</TaskButton> : <></>}
-      {!isEditing ? (
-        <TaskButton
-          type="button"
-          onClick={() => {
-            setIsEditing(!isEditing);
-          }}
-        >
-          編集
+    <div className="flex space-x-3">
+      <form
+        onSubmit={(e) => {
+          onClickSubmit(e);
+        }}
+      >
+        {isEditing ? (
+          <input
+            id={task.id}
+            name={"task"}
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        ) : (
+          <>{title}</>
+        )}
+        {isEditing ? <TaskButton type="submit">保存</TaskButton> : <></>}
+        {!isEditing ? (
+          <TaskButton
+            type="button"
+            onClick={() => {
+              setIsEditing(!isEditing);
+            }}
+          >
+            編集
+          </TaskButton>
+        ) : (
+          <></>
+        )}
+      </form>
+      {!isDone ? (
+        <TaskButton type="button" onClick={onClickDoneTask}>
+          完了
         </TaskButton>
       ) : (
-        <></>
+        <>
+          <p>{task.finishedAt}</p>
+          <TaskButton
+            type="button"
+            onClick={() => {
+              setIsDone(!isDone);
+            }}
+          >
+            タスクを元に戻す
+          </TaskButton>
+        </>
       )}
-    </form>
+    </div>
   );
 };
