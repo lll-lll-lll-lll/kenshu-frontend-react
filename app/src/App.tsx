181d60179f5537dc36824cd8e6f1c838acb73b83
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-05-17 15:47:59 +0900

    feat リスト取得時にSuspense追加

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -14,30 +14,33 @@ const queryClient = new QueryClient();
 export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <MainPage />
+      <Suspense fallback={<Loading />}>
+        <MainPage />
+      </Suspense>
     </QueryClientProvider>
   );
 };
 
 const MainPage: React.FC = () => {
-  const tasks = useQuery({ queryKey: ["tasks"], queryFn: fetchTasks });
+  const tasks = useQuery({
+    queryKey: ["tasks"],
+    queryFn: fetchTasks,
+    suspense: true,
+  });
 
   return (
     <div>
       <TaskCreateButton />
       <h1>Todo List</h1>
       <ul className="flex-row">
-        {tasks.isError && <p>再度リロードしてください</p>}
-        <Suspense fallback={<Loading />}>
-          {tasks.data?.tasks
-            .slice()
-            .reverse()
-            .map((task) => (
-              <li key={task.id}>
-                <Task task={task} />
-              </li>
-            ))}
-        </Suspense>
+        {tasks.data?.tasks
+          .slice()
+          .reverse()
+          .map((task) => (
+            <li key={task.id}>
+              <Task task={task} />
+            </li>
+          ))}
       </ul>
     </div>
   );
